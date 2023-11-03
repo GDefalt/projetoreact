eb1513218f1acacce8d47b6015ac20cd64beeeb2
Dario <dariokleinalves@gmail.com>
2023-11-02 21:17:52 -0300

    semi finalizado o cadastro e login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { FocusProvider } from "./hooks/focus.jsx"
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <FocusProvider>
+      <App />
+    </FocusProvider>
+  </React.StrictMode>
 )
